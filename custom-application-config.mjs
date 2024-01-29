718316e440fe5c17cfaa94ac93e3bfe5e15b5b65
Sai krishna Banda <sai.banda@aspiresys.kl>
2024-01-29 14:17:47 +0530

    initial commit

---
+++ custom-application-config.mjs
@@ -0,0 +1,39 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Starter E4b8d1',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-au',
+  env: {
+    development: {
+      initialProjectKey: 'aspire-practice-project',
+    },
+    production: {
+      applicationId: 'TODO',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
